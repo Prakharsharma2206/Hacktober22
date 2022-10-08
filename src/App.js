a166450ffb752eebb0ab94b97ee4eef17461e911
Ankur-Sh <ankursharma.as0403@gmail.com>
2022-10-08 23:38:48 +0530

    given styling to the cards

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import "./App.css";
 import {
-  AiOutlineTwitter,
-  AiFillLinkedin,
-  AiOutlineGithub,
+    AiOutlineTwitter,
+    AiFillLinkedin,
+    AiOutlineGithub,
 } from "react-icons/ai";
 import { CgWebsite } from "react-icons/cg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Basics of &nbsp;{" "}
-        <strong>
-          <i>Github</i>
-        </strong>
-        &nbsp; and Introduction to &nbsp;{" "}
-        <strong>
-          <i>Hactoberfest</i>
-        </strong>
-      </header>
+    return (
+        <div className="App">
+            <header className="App-header">
+                Basics of &nbsp;{" "}
+                <strong>
+                    <i>Github</i>
+                </strong>
+                &nbsp; and Introduction to &nbsp;{" "}
+                <strong>
+                    <i>Hactoberfest</i>
+                </strong>
+            </header>
 
-      <div className="App-body">
-        {/* 
+            <div className="App-body">
+                {/* 
         <div className="Card">
           <div className="Left-Section">
             <img
@@ -70,236 +70,236 @@ function App() {
         </div>
             */}
 
-        <div className="Card">
-          <div className="Left-Section">
-            <img
-              src="./Images/Prakhar1.jpg"
-              alt="Prakhar"
-              className="profile-image"
-            />
-          </div>
-          <div className="Right-Section">
-            <div>
-              <strong>Name : </strong> Prakhar Sharma
-            </div>
-            <div>
-              <strong>College/Organization : </strong> Maharaja Agrasen
-              Institute of Technology
-            </div>
-            <div>
-              <strong>Year : </strong> 4th Year
-            </div>
-            <div>
-              <strong>Branch : </strong> Computer Science
-            </div>
-            <div className="Social">
-              <strong>Social Links</strong>
-              <div className="icons">
-                <a href="https://twitter.com/prakhar_2206">
-                  <AiOutlineTwitter size={30} />
-                </a>
+                <div className="Card">
+                    <div className="Left-Section">
+                        <img
+                            src="./Images/Prakhar1.jpg"
+                            alt="Prakhar"
+                            className="profile-image"
+                        />
+                        <hr />
+                        <div className="Social">
+                            <div className="icons">
+                                <a href="https://twitter.com/prakhar_2206">
+                                    <AiOutlineTwitter size={30} />
+                                </a>
 
-                <a href="https://www.linkedin.com/in/prakhar-sharma-2206">
-                  <AiFillLinkedin size={30} />
-                </a>
-                <a href="https://github.com/Prakharsharma2206">
-                  <AiOutlineGithub size={30} />
-                </a>
-                <a
-                  href="https://prakharsharma.software"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <CgWebsite size={30} />
-                </a>
-              </div>
+                                <a href="https://www.linkedin.com/in/prakhar-sharma-2206">
+                                    <AiFillLinkedin size={30} />
+                                </a>
+                                <a href="https://github.com/Prakharsharma2206">
+                                    <AiOutlineGithub size={30} />
+                                </a>
+                                <a
+                                    href="https://prakharsharma.software"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <CgWebsite size={30} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Right-Section">
+                        <div>
+                            <strong>Name : </strong> Prakhar Sharma
+                        </div>
+                        <div>
+                            <strong>College/Organization : </strong> Maharaja
+                            Agrasen Institute of Technology
+                        </div>
+                        <div>
+                            <strong>Year : </strong> 4th Year
+                        </div>
+                        <div>
+                            <strong>Branch : </strong> Computer Science
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Card">
+                    <div className="Left-Section">
+                        <img
+                            src="./Images/Kushaggra.jpg"
+                            alt="Your Name"
+                            className="profile-image"
+                        />
+                        <hr />
+                        <div className="Social">
+                            <div className="icons">
+                                <a href="#">
+                                    <AiOutlineTwitter size={30} />
+                                </a>
+
+                                <a href="https://www.linkedin.com/in/kushaggra-sharma/">
+                                    <AiFillLinkedin size={30} />
+                                </a>
+                                <a href="https://github.com/Kushaggra">
+                                    <AiOutlineGithub size={30} />
+                                </a>
+                                <a href="#" target="_blank" rel="noreferrer">
+                                    <CgWebsite size={30} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Right-Section">
+                        <div>
+                            <strong>Name : </strong> Kushaggra Sharma
+                        </div>
+                        <div>
+                            <strong>College/Organization : </strong> Telstra
+                        </div>
+                        <div>
+                            <strong>Year : </strong> 2nd Year
+                        </div>
+                        <div>
+                            <strong>Branch : </strong> Computer Networking
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Card">
+                    <div className="Left-Section">
+                        <img
+                            src="./Images/tiansing.jpg"
+                            alt="tiansing"
+                            className="profile-image"
+                        />
+                        <hr />
+                        <div className="Social">
+                            <div className="icons">
+                                <a href="#">
+                                    <AiOutlineTwitter size={30} />
+                                </a>
+
+                                <a href="https://www.linkedin.com/in/christian-igbaras-41b8a8203/">
+                                    <AiFillLinkedin size={30} />
+                                </a>
+                                <a href="https://github.com/Tiansing">
+                                    <AiOutlineGithub size={30} />
+                                </a>
+                                <a href="#" target="_blank" rel="noreferrer">
+                                    <CgWebsite size={30} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Right-Section">
+                        <div>
+                            <strong>Name : </strong> Christian Igbaras
+                        </div>
+                        <div>
+                            <strong>College/Organization : </strong> SurPathHub
+                        </div>
+                        <div>
+                            <strong>Year : </strong> 4th Year
+                        </div>
+                        <div>
+                            <strong>Branch : </strong> Information Systems
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Card">
+                    <div className="Left-Section">
+                        <img
+                            src="./Images/yagyesh.jpg"
+                            alt="Yagyesh Bobde"
+                            className="profile-image"
+                        />
+                        <hr />
+                        <div className="Social">
+                            <div className="icons">
+                                <a href="https://bit.ly/yb-twitter">
+                                    <AiOutlineTwitter size={30} />
+                                </a>
+
+                                <a href="https://bit.ly/yb-linkedin">
+                                    <AiFillLinkedin size={30} />
+                                </a>
+                                <a href="https://bit.ly/github_yb">
+                                    <AiOutlineGithub size={30} />
+                                </a>
+                                <a
+                                    href="https://bit.ly/yb-linkedin"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <CgWebsite size={30} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Right-Section">
+                        <div>
+                            <strong>Name : </strong> Yagyesh Bobde
+                        </div>
+                        <div>
+                            <strong>College/Organization : </strong>
+                            Anchors
+                        </div>
+                        <div>
+                            <strong>Year : </strong> 1st Year
+                        </div>
+                        <div>
+                            <strong>Branch : </strong> Computer Science
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Card">
+                    <div className="Left-Section">
+                        <img
+                            src="./Images/Vardaan.jpg"
+                            alt="Vardaan Aggarwal"
+                            className="profile-image"
+                        />
+                        <hr />
+                        <div className="Social">
+                            <div className="icons">
+                                <a href="https://twitter.com/VardaanAgg">
+                                    <AiOutlineTwitter size={30} />
+                                </a>
+
+                                <a href="https://www.linkedin.com/in/vardaanaggarwal/">
+                                    <AiFillLinkedin size={30} />
+                                </a>
+                                <a href="https://github.com/VardaanAggarwal">
+                                    <AiOutlineGithub size={30} />
+                                </a>
+                                <a
+                                    href="https://bio.link/vardaanagg"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <CgWebsite size={30} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Right-Section">
+                        <div>
+                            <strong>Name : </strong> Vardaan Aggarwal
+                        </div>
+                        <div>
+                            <strong>College/Organization : </strong>
+                            Vivekananda Institute Of Professional Studies
+                        </div>
+                        <div>
+                            <strong>Year : </strong> 2nd Year
+                        </div>
+                        <div>
+                            <strong>Branch : </strong> BCA
+                        </div>
+                    </div>
+                </div>
+
+                {/* Change the image name and add your image in the Images folder in Public folder */}
+                {/* Copy Line 25 to 70 and paste it below the last card and change the details accordingly. */}
             </div>
-          </div>
         </div>
-
-        <div className="Card">
-          <div className="Left-Section">
-            <img
-              src="./Images/Kushaggra.jpg"
-              alt="Your Name"
-              className="profile-image"
-            />
-          </div>
-          <div className="Right-Section">
-            <div>
-              <strong>Name : </strong> Kushaggra Sharma
-            </div>
-            <div>
-              <strong>College/Organization : </strong> Telstra
-            </div>
-            <div>
-              <strong>Year : </strong> 2nd Year
-            </div>
-            <div>
-              <strong>Branch : </strong> Computer Networking
-            </div>
-            <div className="Social">
-              <strong>Social Links</strong>
-              <div className="icons">
-                <a href="#">
-                  <AiOutlineTwitter size={30} />
-                </a>
-
-                <a href="https://www.linkedin.com/in/kushaggra-sharma/">
-                  <AiFillLinkedin size={30} />
-                </a>
-                <a href="https://github.com/Kushaggra">
-                  <AiOutlineGithub size={30} />
-                </a>
-                <a href="#" target="_blank" rel="noreferrer">
-                  <CgWebsite size={30} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="Card">
-          <div className="Left-Section">
-            <img
-              src="./Images/tiansing.jpg"
-              alt="tiansing"
-              className="profile-image"
-            />
-          </div>
-          <div className="Right-Section">
-            <div>
-              <strong>Name : </strong> Christian Igbaras
-            </div>
-            <div>
-              <strong>College/Organization : </strong> SurPathHub
-            </div>
-            <div>
-              <strong>Year : </strong> 4th Year
-            </div>
-            <div>
-              <strong>Branch : </strong> Information Systems
-            </div>
-            <div className="Social">
-              <strong>Social Links</strong>
-              <div className="icons">
-                <a href="#">
-                  <AiOutlineTwitter size={30} />
-                </a>
-
-                <a href="https://www.linkedin.com/in/christian-igbaras-41b8a8203/">
-                  <AiFillLinkedin size={30} />
-                </a>
-                <a href="https://github.com/Tiansing">
-                  <AiOutlineGithub size={30} />
-                </a>
-                <a href="#" target="_blank" rel="noreferrer">
-                  <CgWebsite size={30} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div className="Card">
-          <div className="Left-Section">
-            <img
-              src="./Images/yagyesh.jpg"
-              alt="Yagyesh Bobde"
-              className="profile-image"
-            />
-          </div>
-          <div className="Right-Section">
-            <div>
-              <strong>Name : </strong> Yagyesh Bobde
-            </div>
-            <div>
-              <strong>College/Organization : </strong> 
-              Anchors
-            </div>
-            <div>
-              <strong>Year : </strong> 1st Year
-            </div>
-            <div>
-              <strong>Branch : </strong> Computer Science
-            </div>
-            <div className="Social">
-              <strong>Social Links</strong>
-              <div className="icons">
-                <a href="https://bit.ly/yb-twitter">
-                  <AiOutlineTwitter size={30} />
-                </a>
-
-                <a href="https://bit.ly/yb-linkedin">
-                  <AiFillLinkedin size={30} />
-                </a>
-                <a href="https://bit.ly/github_yb">
-                  <AiOutlineGithub size={30} />
-                </a>
-                <a
-                  href="https://bit.ly/yb-linkedin"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <CgWebsite size={30} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="Card">
-          <div className="Left-Section">
-            <img
-              src="./Images/Vardaan.jpg"
-              alt="Vardaan Aggarwal"
-              className="profile-image"
-            />
-          </div>
-          <div className="Right-Section">
-            <div>
-              <strong>Name : </strong> Vardaan Aggarwal
-            </div>
-            <div>
-              <strong>College/Organization : </strong>
-              Vivekananda Institute Of Professional Studies
-            </div>
-            <div>
-              <strong>Year : </strong> 2nd Year
-            </div>
-            <div>
-              <strong>Branch : </strong> BCA
-            </div>
-            <div className="Social">
-              <strong>Social Links</strong>
-              <div className="icons">
-                <a href="https://twitter.com/VardaanAgg">
-                  <AiOutlineTwitter size={30} />
-                </a>
-
-                <a href="https://www.linkedin.com/in/vardaanaggarwal/">
-                  <AiFillLinkedin size={30} />
-                </a>
-                <a href="https://github.com/VardaanAggarwal">
-                  <AiOutlineGithub size={30} />
-                </a>
-                <a
-                  href="https://bio.link/vardaanagg"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <CgWebsite size={30} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-           
-        {/* Change the image name and add your image in the Images folder in Public folder */}
-        {/* Copy Line 25 to 70 and paste it below the last card and change the details accordingly. */}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
